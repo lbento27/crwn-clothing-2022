c39f87a4c6adf8051df6e975ad73f9a6dc8905af
Ludgero Bento <ludgerobento@MacBook-Pro-de-Ludgero.local>
2022-02-22 23:13:28 +0000

    updated shop data with new data, updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  ); //if found set cartItem to ExistingCartItem if not returns undefine
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  //if not found set...
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
